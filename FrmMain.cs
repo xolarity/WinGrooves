5423043b1108d66bcd09c254af03c3998ccbfbac
Dan Egbert <dan@xolarity.com>
2011-11-06 16:05:02 -0700

    Disabling the IE click sound

--- FrmMain.cs
+++ FrmMain.cs
@@ -44,6 +44,24 @@ namespace WinGrooves
         private Timer alwaysListeningTimer;
         private ToolStripMenuItem Play;
 
+		//These constants are needed to disable the IE click sound
+        private const int FEATURE_DISABLE_NAVIGATION_SOUNDS = 21;
+        private const int SET_FEATURE_ON_THREAD = 0x00000001;
+        private const int SET_FEATURE_ON_PROCESS = 0x00000002;
+        private const int SET_FEATURE_IN_REGISTRY = 0x00000004;
+        private const int SET_FEATURE_ON_THREAD_LOCALMACHINE = 0x00000008;
+        private const int SET_FEATURE_ON_THREAD_INTRANET = 0x00000010;
+        private const int SET_FEATURE_ON_THREAD_TRUSTED = 0x00000020;
+        private const int SET_FEATURE_ON_THREAD_INTERNET = 0x00000040;
+        private const int SET_FEATURE_ON_THREAD_RESTRICTED = 0x00000080;
+        [DllImport("urlmon.dll")]
+        [PreserveSig]
+        [return: MarshalAs(UnmanagedType.Error)]
+        static extern int CoInternetSetFeatureEnabled(
+        int FeatureEntry,
+        [MarshalAs(UnmanagedType.U4)] int dwFlags,
+        bool fEnable);
+
         public FrmMain()
         {
             //
@@ -305,6 +323,10 @@ namespace WinGrooves
             // register the event that is fired after a key press.
             hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
             webBrowser1.ObjectForScripting = this; //needed to capture JavaScript events
+
+            //disable the IE "click sound"
+            int feature = FEATURE_DISABLE_NAVIGATION_SOUNDS;
+            CoInternetSetFeatureEnabled(feature, SET_FEATURE_ON_PROCESS, true);
 
             if (Properties.Settings.Default.startMinimized)
             {
