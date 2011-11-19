cd7b60f001b81cfaf3038046414c133dcf3c07f5
Dan Egbert <dan@xolarity.com>
2011-11-18 20:33:53 -0700

    Adding the rest of the files needed for allowing the form size and position to be remembered between sessions

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -154,5 +154,29 @@ namespace WinGrooves.Properties {
                 this["startMinimized"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0, 0, 0")]
+        public global::System.Drawing.Rectangle WindowPosition {
+            get {
+                return ((global::System.Drawing.Rectangle)(this["WindowPosition"]));
+            }
+            set {
+                this["WindowPosition"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Normal")]
+        public global::System.Windows.Forms.FormWindowState WindowState {
+            get {
+                return ((global::System.Windows.Forms.FormWindowState)(this["WindowState"]));
+            }
+            set {
+                this["WindowState"] = value;
+            }
+        }
     }
 }
